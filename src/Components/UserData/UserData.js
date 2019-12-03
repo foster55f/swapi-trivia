151f3d52bf10225937d1d53af5cada1908b62a35
Alan Turing <austeyd@gmail.com>
2019-12-03 14:59:22 -0700

    Establish scaffolding for UserData component

---
+++ src/Components/UserData/UserData.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './UserData.css';
+
+const UserData = () => {
+
+  return (
+    <section className="user-data">
+      <p>Placeholder Text, this needs to be reformatted to include buttons, user values, etc (maybe made into a Header element?)</p>
+    </section>
+  )
+};
+
+export default UserData;
