4c761e7d20ad634a6e52167aa0cee4243e1d3a84
Alan Turing <austeyd@gmail.com>
2019-12-03 14:42:32 -0700

    Establish scaffolding for CharacterContainer component

--- src/Components/CharacterCard/CharacterCard.js
+++ src/Components/CharacterCard/CharacterCard.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './CharacterCard.css';
 
 const CharacterCard = () => {
-  
+
   return (
     <article className="character-card">
       <h3 className="character-name">Character Name</h3>
-      <img className="character-img" src={} alt={}/>
-      <div>
+      // <img className="character-img" src={} alt={}/>
+      // Commented out so that empty attribute fields don't cause bugs until we eventually pass props down into this component
+      <div className="character-details">
         <p>Homeworld</p>
         <p>Homeworld Population</p>
         <p>Species</p>
---
+++ src/Components/CharacterContainer/CharacterContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CharacterContainer.css';
+
+const CharacterContainer = () => {
+
+  return (
+    <section className="character-container">
+      <p>Placeholder Element</p>
+    </section>
+  )
+};
+
+export default CharacterContainer;
