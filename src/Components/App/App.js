d90e5e655c4fea7cae4d5a27c029a54f5a9bdf9a
Alan Turing <austeyd@gmail.com>
2019-12-05 08:35:48 -0700

    Add logOut method to App component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,38 +2,45 @@ import React, { Component } from 'react';
 import './App.css';
 import MovieContainer from '../MovieContainer/MovieContainer';
 import Login from '../Login/Login';
-import {retrieveAllMovies} from '../../fetchCalls'
 
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      flicks: [],
-      isSignedIn:false,
-      user: {}
+      user: {
+        name: '',
+        quote: '',
+        ranking: '',
+        favorites: [],
+        clickedFilm: ''
+      }
     }
-  }
-
-  componentDidMount() {
-    retrieveAllMovies('https://swapi.co/api/films/')
-    .then(flicks => this.setState({flicks}))
   }
 
   enterUserInfo = (userData) => {
     this.setState({
-      user: { ...userData },
-      isSignedIn:true
+      user:{...userData}
     })
-    console.log(this.state.user)
   }
+
+  logOut = () => {
+    this.setState({
+      name: '',
+      quote: '',
+      ranking: '',
+      favorites: [],
+      clickedFilm: ''
+    })
+  }
+
 
   render() {
     return (
       <main>
         <div>
-          {/* <Login enterUserInfo={this.enterUserInfo} />   */}
-          <MovieContainer  flicks = {this.state.flicks}/>
+          <Login enterUserInfo = {this.enterUserInfo}/>
+          {/* <MovieContainer foster={['yello', 'dave', 'its', 'all', 'coming', 'together']}/> */}
           </div>
       </main>
     )
