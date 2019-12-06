92ba322c5f049007078f940558ed06965e738d2d
Alan Turing <austeyd@gmail.com>
2019-12-05 23:09:45 -0700

    Resolve merge issue

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,43 +6,51 @@ class Login extends Component {
     constructor() {
         super();
         this.state = {
+            user: {
                 name: '',
                 quote: '',
                 ranking: '',
-                error:''
+                isSignedIn: false,
+                errors: {}
+            }
         }
     }
 
     handleChange(event) {
-        this.setState({ [event.target.name]: event.target.value })       
+        this.setState({ [event.target.name]: event.target.value })
+        console.log(this.state.name)
+        console.log(this.state.quote)
     }
 
     login = (event) => {
         event.preventDefault();
-        const {name,quote,ranking}=this.state
-        if (!name || name.length === 0 || !quote || quote.length === 0) {
-            this.setState({ error: 'Please Fill out all Inputs!!' });
+        if (!this.state.name || this.state.name.length === 0 || !this.state.quote || this.state.quote.length === 0 ) {
+            alert("Please fill out all spaces");
         }
-        this.props.enterUserInfo({ name,quote,ranking})
+        this.setState({ isSignedIn: true })
+        this.props.enterUserInfo(this.state)
     }
+
+    // error = () => {
+
+    // }
 
     render() {
         return (
             <div className = 'login-home'>
-                <h1 className='welcome'>Welcome to Star Wars!!!</h1>
-                <h2>{this.state.error}</h2>
+             <h1 className = 'welcome'>Welcome to Star Wars!!!</h1>
               <form>
                     <input type='text' placeholder='Name' value={this.state.name} name = 'name' onChange={event => this.handleChange(event)} />
                     <textarea type='text' placeholder='Quote' value={this.state.quote} name = 'quote' onChange={event => this.handleChange(event)}></textarea>
                     <div>
                         <label>Select Level</label>
-                        <select value={this.state.ranking} name = 'ranking' onChange={event => this.handleChange(event)}>
+                        <select>
                             <option>padawan</option>
                             <option>jedi knight</option>
                             <option>jedi master</option>
                         </select>
                     </div>
-                    <button type='submit' className='loginButton' onClick ={this.login} >Luke I Am Your Father</button>
+                    <button type='submit' className='loginButton' onClick ={this.login}>Luke I Am Your Father</button>
               </form>
             </div>
         )
