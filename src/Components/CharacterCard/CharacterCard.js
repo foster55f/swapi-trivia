01178c19f2d94bd747f2c459f409f80511205d70
“Foster <fosterataylor@yahoo.com>
2019-12-08 13:31:25 -0700

    Add character card info

--- src/Components/CharacterCard/CharacterCard.js
+++ src/Components/CharacterCard/CharacterCard.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './CharacterCard.css';
 
-const CharacterCard = ({id}) => {
-console.log(id)
+const CharacterCard = ({name,homeworld,population,species,relatedfilms}) => {
+console.log(name)
   return (
     <article className="character-card">
-    <h2 className="movie-name">{id}</h2>
-    {/* <button className="view-characters-button" value={number} id={id} onClick={event=>selectFlick(event.target.id)}>View Characters</button> */}
+      <h2 className="character-name">{name}</h2>
+      <h2 className="character-homeworld">{homeworld}</h2>
+      <h2 className="character-population">{population}</h2>
+      <h2 className="character-species">{species}</h2>
+      <h2 className="character-relatedfilms">{relatedfilms}</h2>
+      <button>View Characters</button>
 </article>
   )
 };
