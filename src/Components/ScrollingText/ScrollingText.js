383e4bef131e0409bcc9e3d9184dada2babf46c3
Alan Turing <austeyd@gmail.com>
2019-12-08 12:33:17 -0700

    Add animation styling for ScrollingText component per online guide

--- src/Components/ScrollingText/ScrollingText.js
+++ src/Components/ScrollingText/ScrollingText.js
@@ -4,10 +4,12 @@ import './ScrollingText.css';
 const ScrollingText = () => {
 
   return (
-    <section className="scrolling-component">
-      <div className="scroll-container">
-        <h4>Specific Episode Placeholder</h4>
-        <p>Scrolling Text</p>
+    <section>
+      <div className="scrolling-container">
+        <div className="scroll-content">
+          <h4>Specific Episode Placeholder</h4>
+          <p>Scrolling Text</p>
+        </div>
       </div>
     </section>
   )
