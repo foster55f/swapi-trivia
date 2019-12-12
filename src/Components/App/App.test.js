7f2e819b6cc5d34661f344ca204e06d7be9e0c09
Alan Turing <austeyd@gmail.com>
2019-12-12 02:32:10 -0700

    Add logOut test

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -12,25 +12,25 @@ describe('App', () => {
     });
   });
 
-  it('should match the snapshot', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper).toMatchSnapshot()
-  });
+    it('should match the snapshot', () => {
+      const wrapper = shallow(<App />);
+      expect(wrapper).toMatchSnapshot()
+    });
 
-  it('should invoke retrieveAllMovies after componentDidMount triggers', () => {
-    shallow(<App />);
-    expect(retrieveAllMovies).toHaveBeenCalled();
-  });
-    //Imports are solid, syntax is fine, but this still invoke in the test (0 invokes)?
-    //Shallow should trigger componentDidMount which invokes retrieveAllMovies
-    //I've tried declaring const retrieveAllMovies = jest.fn.mockimpl() as an alternative with no dice
-    //File pathway isn't the problem
-    //I've tried writing the jest.mock(url with and without the .js suffix)
-    //Jest dependency IS installed; the snapshot test works
-    //I've tried breaking the test into a nested described('componentDidMount' ...), but that didn't work
-    //I tried to first manually invoke componentDidMount (wrapper.instance().componentDidMount();)
-    //Tried playing with async / await in strange ways
-    //Could it have something to do with our class syntax in App.js (App extends React.Component instead of Component)?
+    it('should invoke retrieveAllMovies after componentDidMount triggers', () => {
+      shallow(<App />);
+      expect(retrieveAllMovies).toHaveBeenCalled();
+    });
+      //Imports are solid, syntax is fine, but this still invoke in the test (0 invokes)?
+      //Shallow should trigger componentDidMount which invokes retrieveAllMovies
+      //I've tried declaring const retrieveAllMovies = jest.fn.mockimpl() as an alternative with no dice
+      //File pathway isn't the problem
+      //I've tried writing the jest.mock(url with and without the .js suffix)
+      //Jest dependency IS installed; the snapshot test works
+      //I've tried breaking the test into a nested described('componentDidMount' ...), but that didn't work
+      //I tried to first manually invoke componentDidMount (wrapper.instance().componentDidMount();)
+      //Tried playing with async / await in strange ways
+      //Could it have something to do with our class syntax in App.js (App extends React.Component instead of Component)?
 
 
     it('should update state when enterUserInfo is called', () => {
@@ -48,6 +48,58 @@ describe('App', () => {
     //Test won't register App as a class component (error message: ShallowWrapper::state() can only be called on class components)
     //Tried various unsuccessful tweaks - is there some global issue with the App file? Import syntax?
     //SOLVED - import syntax
+
+    it('should update state when logOut is called', () => {
+      const wrapper = shallow(<App />);
+      const mockUserData = {name: 'Billy Madison', quote: 'Odoyle juuls', ranking: 'Padawan'};
+      // wrapper.instance().enterUserInfo = jest.fn();
+      // wrapper.instance().forceUpdate();
+      // expect(wrapper.state('user')).toEqual(mockUserData);
+      wrapper.instance().setState({user: mockUserData});
+
+      wrapper.instance().logOut();
+
+      expect(wrapper.state()).toEqual(
+                  {
+                    favoritedCharacters: [],
+                    favoritesCounter: 0,
+                    flicks: [],
+                    openingCrawl: null,
+                    selectedCharacters: [],
+                    selectedFlick: {},
+                    user: {
+                      name: "",
+                      quote: "",
+                      ranking: "",
+                    }
+                  });
+    });
+
+
+
+  //
+  //   logOut = () => {
+  //   {/*   this.props.history.push('/')   */}
+  //     this.setState({
+  //       user: {
+  //         name: '',
+  //         quote: '',
+  //         ranking: ''
+  //       },
+  //     })
+  //   }
+  //
+  //
+  //
+  //
+  // selectFlick = (id) => {
+  //   let correctCrawl = this.state.flicks.find(flick => {
+  //     return flick.episode_id === parseInt(id)
+  //   })
+  //   {/*  this.props.history.push(`/movies:${id}`)   */}
+  //   this.setState({ selectedFlick: this.state.flicks[id - 1], openingCrawl: correctCrawl.opening_crawl})
+  //   this.findCharacters(this.state.flicks[id - 1].characters)
+  // }
 
 
 
