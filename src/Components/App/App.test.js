c1140ab3679c53589188a2a8f2583efbaa3a0a32
Alan Turing <austeyd@gmail.com>
2019-12-11 22:55:29 -0700

    Add FIX for now passing handleChange test

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -20,13 +20,14 @@ describe('App', () => {
   it('should invoke retrieveAllMovies after componentDidMount triggers', () => {
     shallow(<App />);
     expect(retrieveAllMovies).toHaveBeenCalled();
-    //Imports are solid, syntax is fine, why won't this invoke in the test?
+    //Imports are solid, syntax is fine, why won't this invoke in the test (0 invokes)?
     //Shallow should trigger componentDidMount which invokes retrieveAllMovies
     //I've tried declaring const retrieveAllMovies = jest.fn.mockimpl() as an alternative with no dice
     //File pathway isn't the problem
     //I've tried writing the jest.mock(url with and without the .js suffix)
-    //Jest dependency IS installed the; snapshot test work
+    //Jest dependency IS installed; the snapshot test works
     //I've tried breaking the test into a nested described('componentDidMount' ...), but that didn't work
     //I tried to first manually invoke componentDidMount (wrapper.instance().componentDidMount();)
+    //Could it have something to do with our class syntax in App.js (App extends React.Component instead of Component)?
   });
 });
--- src/Components/Login/Login.test.js
+++ src/Components/Login/Login.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme'
-import Login from './Login';
+import { Login } from './Login';
 
 describe('Login', () => {
   let wrapper;
@@ -16,15 +16,15 @@ describe('Login', () => {
   it('should update state when handleChange is called', () => {
     const mockEvent = { target: { name: 'name', value: 'foster'} };
     const expected = 'foster';
-    let wrapper2 = shallow(<Login />);
-    wrapper2.instance().handleChange(mockEvent);
+    wrapper.instance().handleChange(mockEvent);
 
-    expect(wrapper2.state('name')).toEqual(expected);
+    expect(wrapper.state('name')).toEqual(expected);
     //Tried relocating let wrapper = shallow(<Login />) locally to each test to no avail
     //Wrapper continues to display as null (can't read handleChange of 'null')
     //Tried chaining the .dive() method to the shallow instance without luck, wrapper is still null
     //Tried to intialize then expectba new variable (let wrapper2 = shallow(<Login />)), which didn't work (still null)
     //Tried tweaking the shallow import syntax
     //Tried removing the initial snapshot test in case it was causing any contamination
+    //SOLVED - sort of, I altered the Login import from (import Login to { Login} , but now the snapshot fails)
   });
 });
