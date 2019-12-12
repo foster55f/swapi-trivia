5550e89bd2e9d4018b4195054c998cbbd7270406
Alan Turing <austeyd@gmail.com>
2019-12-12 02:16:14 -0700

    Solve testing issue with test file regarding import syntax

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import App from './App';
+import { App } from './App';
 import { shallow } from 'enzyme';
 import { retrieveAllMovies } from '../../fetchCalls';
 
 jest.mock('../../fetchCalls.js')
 
 describe('App', () => {
-  let wrapper;
   beforeEach(() => {
-    wrapper = shallow(<App />);
-
     retrieveAllMovies.mockImplementation(() => {
       return Promise.resolve([{movie1: 'name'}, {movie2: 'name'}, {movie3: 'name'}])
     });
   });
 
   it('should match the snapshot', () => {
+    const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot()
   });
 
   it('should invoke retrieveAllMovies after componentDidMount triggers', () => {
     shallow(<App />);
     expect(retrieveAllMovies).toHaveBeenCalled();
-    //Imports are solid, syntax is fine, why won't this invoke in the test (0 invokes)?
+  });
+    //Imports are solid, syntax is fine, but this still invoke in the test (0 invokes)?
     //Shallow should trigger componentDidMount which invokes retrieveAllMovies
     //I've tried declaring const retrieveAllMovies = jest.fn.mockimpl() as an alternative with no dice
     //File pathway isn't the problem
@@ -30,6 +29,8 @@ describe('App', () => {
     //Jest dependency IS installed; the snapshot test works
     //I've tried breaking the test into a nested described('componentDidMount' ...), but that didn't work
     //I tried to first manually invoke componentDidMount (wrapper.instance().componentDidMount();)
+    //Tried playing with async / await in strange ways
     //Could it have something to do with our class syntax in App.js (App extends React.Component instead of Component)?
-  });
+
+
 });
