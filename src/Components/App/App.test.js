2f5370aaa3b514b1dc1614d0e18de3c74b7a4ef7
Alan Turing <austeyd@gmail.com>
2019-12-11 22:12:43 -0700

    Remove quick reference code from test file

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -30,36 +30,3 @@ describe('App', () => {
     //I tried to first manually invoke componentDidMount (wrapper.instance().componentDidMount();)
   });
 });
-
-//
-// selectFlick = (id) => {
-//   let correctCrawl = this.state.flicks.find(flick => {
-//     return flick.episode_id === parseInt(id)
-//   })
-//   this.props.history.push(`/movies:${id}`)
-//   this.setState({ selectedFlick: this.state.flicks[id - 1], openingCrawl: correctCrawl.opening_crawl})
-//   this.findCharacters(this.state.flicks[id - 1].characters)
-// }
-//
-// findCharacters(characters) {
-//   retrieveAllCharacters(characters.slice(0,10))
-//     .then(response => this.setState({ selectedCharacters: response }))
-//     .then(()=> this.forceUpdate())
-// }
-//
-// enterUserInfo = (userData) => {
-//   this.setState({
-//     user: { ...userData },
-//   })
-// }
-//
-// logOut = () => {
-//   this.props.history.push('/')
-//   this.setState({
-//     user: {
-//       name: '',
-//       quote: '',
-//       ranking: ''
-//     },
-//   })
-// }
